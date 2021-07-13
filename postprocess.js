837dbdd9a60a6fe566661d57690751170a700a91
CHOI HEE JAE <gmlwo530@gmail.com>
2021-07-13 22:21:29 +0900

    Change url to steam spy

--- postprocess.js
+++ postprocess.js
@@ -1,7 +1,3 @@
-// This can be a typescript file as well
-
-// Helper library written for useful postprocessing tasks with Flat Data
-// Has helper functions for manipulating csv, txt, json, excel, zip, and image files
 import {
   readJSON,
   writeJSON,
@@ -11,19 +7,22 @@ import {
 // Step 1: Read the downloaded_filename JSON
 const filename = Deno.args[0]; // Same name as downloaded_filename `const filename = 'btc-price.json';`
 const json = await readJSON(filename);
-console.log(json);
 
-// Step 2: Filter specific data we want to keep and write to a new JSON file
-const currencyRates = Object.values(json.bpi); // convert property values into an array
-const filteredCurrencyRates = currencyRates.map((rate) => ({
-  currency: rate.description,
-  bitcoinRate: rate.rate,
+const values = Object.values(json); // convert property values into an array
+const filteredValues = values.map((val) => ({
+  appid: val.appid,
+  name: val.name,
+  owners: val.owners,
+  average_forever: val.average_forever,
+  average_2weeks: val.average_2weeks,
+  median_forever: val.median_forever,
+  median_2weeks: val.median_2weeks,
+  ccu: val.ccu,
 }));
 
 // Step 3. Write a new JSON file with our filtered data
 const newFilename = `steam-postprocessed.json`; // name of a new file to be saved
-await writeJSON(newFilename, filteredCurrencyRates); // create a new JSON file with just the Bitcoin price
-console.log("Wrote a post process file");
+await writeJSON(newFilename, filteredValues); // create a new JSON file with just the Bitcoin price
 
 // Optionally delete the original file
 await removeFile("./steam.json"); // equivalent to removeFile('btc-price.json')
